c5ae2b571f4f9b772596e474c87dc76fa07be2ad
Paulo Rabelo <pauloeduardorabelomiranda@gmail.com>
2024-02-06 21:54:44 -0300

    atvoperadorlogexclusivo.c

--- atvoperadorlogexclusivo.c
+++ atvoperadorlogexclusivo.c
@@ -7,21 +7,22 @@ int main() {
     printf("Prato do dia: Peixe acompanhado de Arroz Ou Salada\n");
 
     // Pede os pedidos do cliente
-    printf("Digite 1 se quiser peixe, 1 se quiser arroz, 1 se quiser salada (0 para n„o escolher):\n");
+    printf("Digite 1 se quiser peixe, 1 se quiser arroz, 1 se quiser salada (0 para n√£o escolher):\n");
 
-    // LÍ os pedidos
+    // L√™ os pedidos
     scanf("%i %i %i", &peixe, &arroz, &salada);
 
     // Verifica se o peixe foi escolhido
     if (peixe) {
-        // Verifica se escolheu arroz ou salada, mas n„o ambos
+        // Verifica se escolheu arroz ou salada, mas n√£o ambos
         if ((arroz ^ salada) && !(arroz && salada)) {
-            printf("Pedido v·lido.\n");
-        } else {
-            printf("Pedido inv·lido. Escolha arroz ou salada, mas n„o ambos.\n");
+            printf("Pedido v√°lido.\n");
+        } 
+        else {
+            printf("Pedido inv√°lido. Escolha arroz ou salada, mas n√£o ambos.\n");
         }
     } else {
-        printf("Pedido inv·lido. Peixe È obrigatÛrio.\n");
+        printf("Pedido inv√°lido. Peixe √© obrigat√≥rio.\n");
     }
 
     return 0;
